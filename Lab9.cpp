e0a3d25688d91e0a041d0416cf2d5eeef3eedd6e
AlexanderChernykh <sasha2012773@yandex.ru>
2020-06-29 09:12:15 +0300

    Закончил работу над лабораторной работой №9

--- Lab9.cpp
+++ Lab9.cpp
@@ -44,12 +44,95 @@ void output_matr(int** p, int n)
 
 void delete_p(int** p, int n)
 {
-    int i;
-    for (i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         delete[] p[i];
     }
     delete[] p;
+}
+
+void work(int** p, int n, int** c)
+{
+    bool AllNumbersLeftSmaller = false;
+    bool AllNumbersRightBigger = false;
+    bool NoNumbersInRow = false;
+
+    c = new int* [2];
+    for (int i = 0; i < n; i++)
+        c[i] = new int[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        NoNumbersInRow = true;
+        for (int j = 0; j < n; j++)
+        {
+            if ((j > 0) and (j < n - 1))
+            {
+                int x = p[i][j];
+                for (int k = 0; k < n; k++)
+                {
+                    int x_2 = p[i][k];
+                    if (k < j)
+                    {
+                        if (x > x_2)
+                        {
+                            AllNumbersLeftSmaller = true;
+                        }
+                        else
+                        {
+                            AllNumbersLeftSmaller = false;
+                        }
+                    }
+
+                    else if (k > j)
+                    {
+                        if (x < x_2)
+                        {
+                            AllNumbersRightBigger = true;
+                        }
+                        else
+                        {
+                            AllNumbersRightBigger = false;
+                        }
+                    }
+                }
+
+                if ((AllNumbersLeftSmaller) and (AllNumbersRightBigger))
+                {
+                    c[0][j] = -1;
+                    c[1][j] = x;
+                    NoNumbersInRow = false;
+                }
+                else
+                {
+                    c[0][j] = 0;
+                    c[1][j] = x;
+                }
+            }
+        }
+
+        if (NoNumbersInRow)
+        {
+            cout << i << " строке нет числа, удовлетвор€ющего условию";
+        }
+
+        else
+        {
+            cout << i << " строке есть число, удовлетвор€ющее условию";
+
+            for (int i = 0; i < n; i++)
+            {
+
+                if (c[0][i] == -1)
+                {
+                    cout << "\n" << "„исло, удовлетвор€ющее условию: " << c[1][i];
+                }
+
+            }
+        }
+        cout << "\n";
+    }
+
 }
 
 int main()
@@ -59,11 +142,10 @@ int main()
     cout << "¬ведите размерность матрицы: ";
     cin >> n;
     int** p = NULL;
+    int** c = NULL;
     input_matr(p, n);
     cout << "¬веденный массив: \n";
     output_matr(p, n);
-    //work(p, n);
-    cout << "ћодифицированный массив: \n";
-    output_matr(p, n);
+    work(p, n, c);
     delete_p(p, n);
 }
